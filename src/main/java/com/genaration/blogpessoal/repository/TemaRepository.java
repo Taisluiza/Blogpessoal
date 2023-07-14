ad11307432a9b36d53629089189dd937f39a6e9f
Taisluiza <taissalissousa@gmail.com>
2023-07-14 08:38:05 -0300

    Security e teste unitario

--- src/main/java/com/genaration/blogpessoal/repository/TemaRepository.java
+++ src/main/java/com/genaration/blogpessoal/repository/TemaRepository.java
@@ -3,6 +3,7 @@ package com.genaration.blogpessoal.repository;
 import java.util.List;
 
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
---
+++ src/main/java/com/genaration/blogpessoal/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.genaration.blogpessoal.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.genaration.blogpessoal.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+}
